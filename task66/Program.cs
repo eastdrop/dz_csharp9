131f6cb3062c4205b16288e97704c7d894764da9
Eastdrop QQ <lyokha7777@gmail.com>
2023-03-14 22:21:43 +0300

    added task66

---
+++ task66/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30*/
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumFromMToN(int m, int n)
+{
+    if (n == m - 1) return 0;
+    return n + SumFromMToN(m, n - 1);
+}
+
+int m = ReadInt("Inp number m");
+int n = ReadInt("Inp number n");
+
+if (m < n)
+{
+    Console.WriteLine(SumFromMToN(m, n));
+}
+else Console.WriteLine(SumFromMToN(n, m));
